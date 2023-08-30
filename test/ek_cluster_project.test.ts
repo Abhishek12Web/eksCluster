8bb2fe38f1986a5d31a2f5700d55dce827328fdd
Abhishek Bharadwaj <abhishekbharadwaj1992@gmail.com>
2023-08-30 17:32:30 +0200

    eks repo setup

---
+++ test/ek_cluster_project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EkClusterProject from '../lib/ek_cluster_project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ek_cluster_project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EkClusterProject.EkClusterProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
